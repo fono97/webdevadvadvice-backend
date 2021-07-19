2afd56ffb9595f9cd250a1c919ed118b2380f866
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-07-19 10:16:19 -0400

    I found out the difference between heroku local npm start and strapi develop

--- config/database.js
+++ config/database.js
@@ -1,27 +1,7 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST', '@cluster0.jfoaf.mongodb.net'),
-//         srv: env.bool('DATABASE_SRV', true),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'webdevadvice-backend'),
-//         username: env('DATABASE_USERNAME', 'admin'),
-//         password: env('DATABASE_PASSWORD', 'Gabriel123@!?'),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', true),
-//       },
-//     },
-//   },
-// });
 
 module.exports = ({ env }) => {
+  console.log("i am a real dev")
     if(env('NODE_ENV') === "development" ) {
-      console.log('I am a developer')
     return {
       
        defaultConnection: 'default',
@@ -41,7 +21,9 @@ module.exports = ({ env }) => {
 
 }
 else {
+  console.log("i am not  a real dev")
 return {
+  
   defaultConnection: 'default',
   connections: {
     default: {
