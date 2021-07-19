03ab046e951897ce09194b0e5f2e3b6a30c9f487
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2021-07-18 21:27:29 -0400

    I added two datadase sqli && mongo

--- config/database.js
+++ config/database.js
@@ -40,27 +40,27 @@ module.exports = ({ env }) => {
   }
 
 }
-// else {
-// return {
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'mongoose',
-//       settings: {
-//         host: env('DATABASE_HOST', '@cluster0.jfoaf.mongodb.net'),
-//         srv: env.bool('DATABASE_SRV', true),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'webdevadvice-backend'),
-//         username: env('DATABASE_USERNAME', 'admin'),
-//         password: env('DATABASE_PASSWORD', 'Gabriel123@!?'),
-//       },
-//       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', true),
-//       },
-//     },
-//   },
-// }
-// }
+else {
+return {
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '@cluster0.jfoaf.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'webdevadvice-backend'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'Gabriel123@!?'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+}
+}
 
 };
